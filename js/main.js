f21d4040056e9d70504a23a02d0dea58176fb246
Emmagold01 <123802753+Emmagold01@users.noreply.github.com>
2023-05-25 16:52:34 +0100

    update contact code

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const form = document.querySelector(".handleSubmit")
+const fname = document.querySelector("#first-name")
+const lname = document.querySelector("#last-name")
+const email = document.querySelector("#email")
+const msg = document.querySelector("#msg")
+
+function handleSubmit(e) {
+    e.preventDefault()
+    firstName = fname.value
+    lastName = lname.value
+    emailName = email.value
+    message = msg.value
+    
+    form.reset();
+    Swal.fire(
+        'Good job!',
+        'Submitted Successfully!!!',
+        'success'
+    )
+}
+
+form.addEventListener('submit', handleSubmit)
